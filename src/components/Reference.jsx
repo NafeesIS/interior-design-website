472628fa1167d8e69687945e890d3238ad3e7386
NafeesIS <nafeesshawon555@gmail.com>
2023-11-23 23:59:04 +0600

    components added

---
+++ src/components/Reference.jsx
@@ -0,0 +1,42 @@
+// local imports
+
+import imageOne from "../../public/image-one.svg";
+import imageTwo from "../../public/image-two.svg";
+import imageThree from "../../public/image-three.svg";
+
+import { referenceSubtitle, referenceTitle } from "@/utils/data";
+import FadeIn from "@/utils/FadeIn";
+import Image from "next/image";
+
+const Reference = () => {
+  return (
+    <div id="reference" className="mb-[160px] px-10 max-w-[1490px] mx-auto">
+      <FadeIn delay={0.2} direction="down">
+        <h1 className="text-5xl lg:text-[64px] font-medium text-fontBlack mb-6 text-center">
+          {referenceTitle}
+        </h1>
+      </FadeIn>
+      <FadeIn delay={0.4} direction="down">
+        <h5 className="text-[#4F4F4F] text-lg xs:text-xl mb-12 text-center">
+          {referenceSubtitle}
+        </h5>
+      </FadeIn>
+
+      <div className="flex flex-col md:flex-row md:justify-center gap-8">
+        <FadeIn delay={0.2} direction="right">
+          <div className="flex flex-col gap-8">
+            <Image src={imageOne} alt="" />
+            <Image src={imageTwo} alt="" />
+          </div>
+        </FadeIn>
+        <FadeIn delay={0.2} direction="left">
+          <div>
+            <Image src={imageThree} alt="" />
+          </div>
+        </FadeIn>
+      </div>
+    </div>
+  );
+};
+
+export default Reference;
